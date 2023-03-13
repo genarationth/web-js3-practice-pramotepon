dd871c72cc0e570b679759741e84f6979f6420f9
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-13 16:30:39 +0700

    Comment code

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Function add add parameters a,b for output a + b
 const add = (a, b) => console.log(a + b);
+// Function subtract add parameters a,b for output a - b
 const subtract = (a, b) => console.log(a - b);
+// Function multiply add parameters a,b for output a * b
 const multiply = (a, b) => console.log(a * b);
+// Function divide add parameters a,b for output a / b
 const divide = (a, b) => console.log(a / b);
+// Call function add add Argument a,b
+add(5, 5);
+// Call function subtract add Argument a,b
+subtract(5, 5);
+// Call function multiply add Argument a,b
+multiply(5, 5);
+// Call function divide add Argument a,b
+divide(5, 5);
 
-add(5,5);
-subtract(5,5);
-multiply(5,5);
-divide(5,5);
-
+// Function inForm add parameters firstName, location, hobby
 const inForm = (firstName, location, hobby) => console.log(`Hi, my name is ${firstName} I live in ${location} and enjoy ${hobby}`);
+// Call function inForm add Argument firstName, location, hobby
 inForm("Pramote", "bangkok", "listen to music");
