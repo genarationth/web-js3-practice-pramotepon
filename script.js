653f47849803ca059edcb2168f1de5509122642b
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-13 16:23:21 +0700

    Completed

---
+++ script.js
@@ -0,0 +1,12 @@
+const add = (a, b) => console.log(a + b);
+const subtract = (a, b) => console.log(a - b);
+const multiply = (a, b) => console.log(a * b);
+const divide = (a, b) => console.log(a / b);
+
+add(5,5);
+subtract(5,5);
+multiply(5,5);
+divide(5,5);
+
+const inForm = (firstName, location, hobby) => console.log(`Hi, my name is ${firstName} I live in ${location} and enjoy ${hobby}`);
+inForm("Pramote", "bangkok", "listen to music");
